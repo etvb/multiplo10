2e381be689d0f60fa328aafdbfd38e71648c9cc9
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-09-04 06:26:09 -0500

    agregamso las variables

--- PATS-20-1-multiplo10/src/pats/pkg20/pkg1/multiplo10/PATS201Multiplo10.java
+++ PATS-20-1-multiplo10/src/pats/pkg20/pkg1/multiplo10/PATS201Multiplo10.java
@@ -5,6 +5,8 @@
  */
 package pats.pkg20.pkg1.multiplo10;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author edgardo
@@ -15,7 +17,29 @@ public class PATS201Multiplo10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int numero;
+        
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero"))%10 ;
+        /*
+        if(numero == 0){
+            JOptionPane.showMessageDialog(null, "Es multiplo de 10");
+        }else{
+            JOptionPane.showMessageDialog(null, "No es multiplo de 10");
+        }
+        */
+        
+        //Con SWITCH
+        
+        switch(numero){
+            case 0:
+                JOptionPane.showMessageDialog(null,"Es multiplo de 10");
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "No es multiplo de 10");
+        }
+
+        
     }
     
 }
