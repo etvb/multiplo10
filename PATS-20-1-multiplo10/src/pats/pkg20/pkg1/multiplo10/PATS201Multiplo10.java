335ec738803a43fe6f04d4d7584fae8ceb831ff5
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-09-04 06:06:34 -0500

    archivos base

---
+++ PATS-20-1-multiplo10/src/pats/pkg20/pkg1/multiplo10/PATS201Multiplo10.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pats.pkg20.pkg1.multiplo10;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PATS201Multiplo10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
